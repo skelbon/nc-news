97d7dec983797329b858fe0793e0c6714467383e
Joe Gibson <spuriousmeaning@gmail.com>
2023-10-24 14:05:45 +0100

    Completed list all articles as home route

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
+import * as React from 'react'
 import { useState } from 'react'
 import './App.css'
 import {Box, CssBaseline, Toolbar} from "@mui/material";
 import ButtonAppBar from './components/NavBar'
 import {Route, Routes} from "react-router-dom";
 import Articles from './components/Articles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import RecipeReviewCard from './components/ArticleComplexCard';
+
 
 function App() {
+  
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
+  
+  
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <ThemeProvider theme={theme}>
+      
       <CssBaseline/>
       <ButtonAppBar/>
       <Box component="main" sx={{p: 3, margin: "auto", padding: "auto", width: "90%", marginTop: 4}}>
@@ -18,6 +39,7 @@ function App() {
           <Route path="/" element= {<Articles />}/>
         </Routes>
       </Box>
+      </ThemeProvider>
     </>
   )
 }
--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,27 +1,31 @@
+
 import { Typography, Grid } from "@mui/material"
 import { useEffect, useState } from "react"
-import ArticleCard from "./ArticleCard"
+import ArticleCard from "./ArticleComplexCard"
 
 
-
+import * as React from 'react';
 export default function Articles(){
     
     const [loading, setLoading] = useState(true);
     const [articles, setArticles] = useState([])
+    const [users, setUsers] = useState({})
     
     useEffect(()=>{
-
+            
         const fetchArticles = async () =>{
             const articlesResponse = await fetch("https://skelbon-news-api.onrender.com/api/articles")
             const articles = await articlesResponse.json()
-            console.log(articles)
+            const usersResponse = await fetch(`https://skelbon-news-api.onrender.com/api/users`)
+            const users = await usersResponse.json()
+            setUsers(await users)
             setArticles(articles)
             setLoading(false)
         }
          fetchArticles()
     }, [])
     
-    if (loading) return (<div>Loading</div>)
+    if (loading) return (<div>Loading. Please be patient, this can take a while while the server spins up if it's been idle for a while...</div>)
 
     
     return (
@@ -30,8 +34,8 @@ export default function Articles(){
         {
             articles.map((article)=>{
                 return (
-                    <Grid item xs={12} sm={6} md={4} lg={3} key={article.article_id} style={{padding:'3px'}}>
-                        <ArticleCard  article={article}/>
+                    <Grid item xs={12} sm={12} md={12} lg={12} key={article.article_id} style={{padding:'3px'}}>
+                        <ArticleCard  article={article} users={users}/>
                     </Grid>
                 )
             })
