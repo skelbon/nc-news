e9189557c2a71f2b3df7cddfffb29686d17f08f4
Joe Gibson <spuriousmeaning@gmail.com>
2023-10-25 13:53:19 +0100

    added optimistically rendered article voting

---
+++ src/components/NetworkErrorAlert.jsx
@@ -0,0 +1,37 @@
+import {useState} from 'react';
+import Box from '@mui/material/Box';
+import Alert from '@mui/material/Alert';
+import IconButton from '@mui/material/IconButton';
+import Collapse from '@mui/material/Collapse';
+import Button from '@mui/material/Button';
+import CloseIcon from '@mui/icons-material/Close';
+
+export default function NetworkAlert({message, severity, setIsVoteError}) {
+  const [open, setOpen] = useState(true);
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Collapse in={open}>
+        <Alert
+        severity={severity}
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={() => {
+                setOpen(false);
+                setIsVoteError(null)
+              }}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+          sx={{ mb: 2 }}
+        >
+          {`${message}`}
+        </Alert>
+      </Collapse>
+    </Box>
+  );
+}
